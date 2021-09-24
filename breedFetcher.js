e89453f0ec9cfb33c89cebaefcc2af4ab241fb1b
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-24 04:55:54 -0400

    Mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,24 @@
 const request = require('request');
 const fs = require('fs');
 
-const breedName = process.argv[2];
-const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+//const breedName = process.argv[2]; --> if using non mocha
+//const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(domain, (error, response, body) => {
-  const data = JSON.parse(body);
-  
-  fs.readFile(body, 'utf8', () => { //use readFile to read the data
-    if (error) {
-      console.log('breed is not found!'); // If error
-    } if (data.length === 0) {
-      console.log('breed is not found!'); // If no breed means no data
-    } else {
-      console.log(data[0]['description']); // if there is breed
-    }
-  }
-  );
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
+    const data = JSON.parse(body);
+    const desc = data[0]['description'];
+
+    fs.readFile(body, 'utf8', () => { //use readFile to read the data
+      if (error) {
+        callback(error, null);
+      } if (data.length === 0) {
+        callback(error, null);
+      } else {
+        callback(null, desc);
+      }
+    });
+  });
+};
+
+module.exports = { fetchBreedDescription };
